65f0cfd492a059bce6c3d8cc73a44ec303404d87
Jeremy Smith <st.jeremy1@gmail.com>
2022-09-28 08:30:01 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
     return (
         <div className="Navbar">
             <h1>NeoPlux Swim Academy</h1>
             <Link to='/'>Home</Link>
             <Link to="/Register">Register</Link>
+            
             <Link to="/ContactUs">Contact Us</Link>
             <input type="search" name="Search" id="search" placeholder="Search" />
         </div>
