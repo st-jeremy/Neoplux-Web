c029ff536329c24fbbb0a02d9bf70076d4837779
Jeremy Smith <st.jeremy1@gmail.com>
2022-09-28 08:21:13 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,19 @@ import ContentList from './ContentList';
 import useFetch from './useFetch';
 
 const Home = () => {
-    const { data: content, isLoading, error } = useFetch('http://localhost:8000/Content') 
+    const { data: content, isLoading, error } = useFetch('http://localhost:5000/Content') 
     
     return ( 
         <div className="Home column">
-
-            { <img src={pic} alt="swim" />}
-
             {/* Output the right if the left is returned as true */}
+            
             { isLoading && <div> Please, wait!<br/> <img src={gif} alt='loading' /></div>}
 
             { error && <div>{ error }</div>}
 
             {content && <ContentList content={content} title="Swimming" />}
+
+            { <img src={pic} alt="swim" />}
 
         </div>
     );
