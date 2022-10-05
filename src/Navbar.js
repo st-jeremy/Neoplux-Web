cdbfae1ffaa899e32a9fa0912bf114e34c04ef86
Jeremy Smith <st.jeremy1@gmail.com>
2022-10-05 07:43:37 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
+import Logo from './Images/logo-removebg.png';
 
 const Navbar = () => {
 
+
     return (
         <div className="Navbar">
-            <h1>NeoPlux Swim Academy</h1>
-            <Link to='/'>Home</Link>
-            <Link to="/Register">Register</Link>
+            <div className="Nav1">
+                <img src= {Logo} alt="logo" id="Logo" className="Logo"/>
+                <h1>NeoPlux Swim Academy</h1>
+            </div>
             
-            <Link to="/ContactUs">Contact Us</Link>
-            <input type="search" name="Search" id="search" placeholder="Search" />
+            <div className="Nav2">
+                <Link to='/'>Home</Link>
+                <Link to="/Register">Register</Link>
+                <Link to="/ContactUs">Contact Us</Link>
+                <Link to="AboutUs">About Us</Link>
+                <Link to="Trainers">Meet the Trainers</Link>
+                <br></br>
+                <input type="search" name="Search" id="search" placeholder="  Search" />
+            </div>
+            
         </div>
     );
 }
