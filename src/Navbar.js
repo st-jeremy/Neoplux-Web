c45e834f5e5401caceaa6cc30647b1e0ef08f681
Jeremy Smith <st.jeremy1@gmail.com>
2022-10-06 21:31:18 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
 import Logo from './Images/logo-removebg.png';
+// import { FcSearch } from "react-icons/fc";
 
 const Navbar = () => {
-
+    const linkStyle = {
+        color: 'maroon',
+        borderRadius: '10px'
+    }
 
     return (
         <div className="Navbar">
             <div className="Nav1">
                 <img src= {Logo} alt="logo" id="Logo" className="Logo"/>
+                <br />
                 <h1>NeoPlux Swim Academy</h1>
             </div>
             
             <div className="Nav2">
-                <Link to='/'>Home</Link>
-                <Link to="/Register">Register</Link>
-                <Link to="/ContactUs">Contact Us</Link>
-                <Link to="AboutUs">About Us</Link>
-                <Link to="Trainers">Meet the Trainers</Link>
-                <br></br>
+                <Link to='/' style={linkStyle}>Home</Link>
+                <Link to="/Register" style={linkStyle}>Register</Link>
+                <Link to="/ContactUs" style={linkStyle}>Contact Us</Link>
+                <Link to="AboutUs" style={linkStyle}>About Us</Link>
+                <br />
                 <input type="search" name="Search" id="search" placeholder="  Search" />
             </div>
             
