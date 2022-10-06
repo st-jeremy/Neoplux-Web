a6eaee58188f3a6cde021c75b9d981a9e34a4300
Jeremy Smith <st.jeremy1@gmail.com>
2022-10-06 21:29:56 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,43 @@
-import pic from './swim2.png';
-import gif from './loading1.gif';
+import pic from './Images/swim2.png';
+import gif from './Images/loading.gif';
+import arena from "./Images/arenalogo.png";
+import speedo from "./Images/speedologo.png";
+import omega from "./Images/omegalogo.png";
 import ContentList from './ContentList';
+import Random from './Random';
 import useFetch from './useFetch';
+import './index.css';
 
 const Home = () => {
-    const { data: content, isLoading, error } = useFetch('http://localhost:5000/Content') 
-    
+    const { data: content, isLoading, error } = useFetch('http://localhost:8000/Content');
+
+     
     return ( 
-        <div className="Home column">
+        <div className="Home">
+
+            <div className='random'>
+                <Random />
+            </div>
+
+            
+
             {/* Output the right if the left is returned as true */}
             
-            { isLoading && <div> Please, wait!<br/> <img src={gif} alt='loading' /></div>}
+            { isLoading && <div><img src={gif} alt='loading' id='loading'/></div>}
 
             { error && <div>{ error }</div>}
 
             {content && <ContentList content={content} title="Swimming" />}
 
-            { <img src={pic} alt="swim" />}
+            <div className="Partners">
+                <h3>Our Partners</h3>
+                <img src={arena} alt="Arena" />
+                <img src={omega} alt="omega" />
+                <img src={speedo} alt="speedo" />
 
+            </div>
+
+            <img src= {pic} alt='Pic' id='homePic' />
         </div>
     );
 }
